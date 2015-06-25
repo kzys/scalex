65d51dcd5126fd4b68b57ca98cbbc2df6e934053
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2015-06-24 21:11:32 -0700

    Use Scalex itself as testing

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 set -e
+
+FAT_JAR=target/scala-2.11/scalex-assembly-3.0-SNAPSHOT.jar dest=~ec2-user/scalex-assembly-3.0-SNAPSHOT.jar
+
 sbt assembly
+
+java -Dscala.usejavacp=true -jar $FAT_JAR index \
+    --name scalex --version 3.0.0 --directory src/main/scala
+
 ansible-playbook ansible/update.yml "$@"
